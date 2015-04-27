c93748768dec60120e60721d24fa160587447f26
techno <tecykobayashi@gmail.com>
2015-04-27 17:37:33 +0900

    html5mode false

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -19,7 +19,7 @@ angular
     'userServices'
   ])
   .config(function ($routeProvider, $locationProvider) {
-    $locationProvider.html5Mode(true);
+    //$locationProvider.html5Mode(true);
     $routeProvider
       .when('/', {
         redirectTo: '/user'
